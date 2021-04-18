e2f8faa6f84ae3d5c4d28437a55022f9858cf99a
김혁 <kimhyuk@gimhyeog-ui-MacBookPro.local>
2021-04-18 10:21:29 +0900

    페이지 리스트 가져오기 함수와 페이지 체크 함수 구현

--- Java Resource/tzs/TzsDAO.java
+++ Java Resource/tzs/TzsDAO.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 
 public class TzsDAO {
 		private Connection conn; 
@@ -70,6 +71,52 @@ public class TzsDAO {
 				e.printStackTrace();
 			}
 			return -1;
+		}
+		
+		
+		// 게시글 리스트 반환 함수
+		public ArrayList<Tzs> getList(int pageNumber){
+			String SQL = "SELECT * FROM TZS where tzsID < ? and tzsAvailable = 1 order by tzsID decs limit 10";
+			ArrayList<Tzs> list = new ArrayList<>();
+			try {
+				PreparedStatement pstmt = conn.prepareStatement(SQL);
+				pstmt.setInt(1, getNext() - (pageNumber - 1) * 10); 
+				// 위에서 만든 getNext()(다음으로 작성될 글의 번호를 부여하는 함수)
+				// 에다가 
+				rs = pstmt.executeQuery();
+				while(rs.next()) {
+					Tzs tzs = new Tzs();
+					tzs.setTzsID(rs.getInt(1));
+					tzs.setTzsTitle(rs.getString(2));
+					tzs.setUserID(rs.getString(3));
+					tzs.setTzsDate(rs.getString(4));
+					tzs.setTzsContent(rs.getString(5));
+					tzs.setTzsAvailable(rs.getInt(6));
+					list.add(tzs); // 뽑아온 데이터가 담긴 tzs를 list에 넣는다.
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return list;
+		}
+		
+		
+		// 페이징 처리 함수 
+		// 글을 10개까지 보여준다고 했을때 11번째 글이 없다면 
+		// 다음 페이지 버튼이 없어야하기때문에 그 체크를 이 함수가 해준다. 
+		public boolean nextPage(int pageNumber) {
+			String SQL = "SELECT * FROM TZS where tzsID < ? and tzsAvailable = 1";
+			try {
+				PreparedStatement pstmt = conn.prepareStatement(SQL);
+				pstmt.setInt(1, getNext() - (pageNumber - 1) * 10); 
+				rs = pstmt.executeQuery();
+				if(rs.next()) {
+					return true;
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return false;
 		}
 }
 
